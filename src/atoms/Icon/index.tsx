2921e790ed25e6bc474b40963ed525b1eb290db2
Roy <roy.appeldoorn@gmail.com>
2023-08-31 11:56:56 +0200

    Add outline variant to icons

--- src/atoms/Icon/index.tsx
+++ src/atoms/Icon/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import classNames from 'classnames';
+import React from "react";
+import classNames from "classnames";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
-export type IconProps = JSX.IntrinsicElements['span'] & {
-    name: string;
-}
+export type IconProps = JSX.IntrinsicElements["span"] & {
+  name: string;
+  fill?: boolean;
+};
 
-const Icon: React.FC<IconProps> = ({ className, name }) => (
-    <span className={ classNames('material-symbols-rounded', styles.icon, className) }>
-        { name }
-    </span>
+const Icon: React.FC<IconProps> = ({ className, name, fill = true }) => (
+  <span
+    className={classNames(
+      fill ? "material-symbols-rounded" : " material-symbols-rounded-outlined",
+      styles.icon,
+      className
+    )}
+  >
+    {name}
+  </span>
 );
 
 export default Icon;
